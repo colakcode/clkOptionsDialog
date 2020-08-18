23fe1b2695df60cb42b5b17effd06f0f345eb2a2
clk3508 <kemallcolak@gmail.com>
2020-08-18 22:11:36 +0300

    Initial commit for Library

---
+++ OptionsDialog/src/main/java/com/clk/optionsdialog/DialogMethods.java
@@ -0,0 +1,60 @@
+package com.clk.optionsdialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class DialogMethods {
+
+    public static void setWidth(Activity activity, Dialog dialog){
+        DisplayMetrics metrics=activity.getResources().getDisplayMetrics();
+        int w=metrics.widthPixels;
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setLayout((w*95)/100, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+    }
+
+    public static void setFullWidth(Activity activity, Dialog dialog){
+        DisplayMetrics metrics=activity.getResources().getDisplayMetrics();
+        int w=metrics.widthPixels;
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setLayout(w, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+    }
+
+    public static void setAnimation(Activity activity, RelativeLayout layout){
+        Animation anim = AnimationUtils.loadAnimation(activity, R.anim.custom_window);
+        anim.reset();
+        layout.clearAnimation();
+        layout.startAnimation(anim);
+    }
+
+    public static void setAnimation(Activity activity, LinearLayout layout){
+        Animation anim = AnimationUtils.loadAnimation(activity, R.anim.custom_window);
+        anim.reset();
+        layout.clearAnimation();
+        layout.startAnimation(anim);
+    }
+
+    public static void setGravity(Dialog dialog , int gravity){
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+
+        // gravity= Gravity.BOTTOM, Gravity.TOP vs
+        wlp.gravity=gravity;
+
+        // to make lightest background of dialog
+        //wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        window.setAttributes(wlp);
+    }
+}
---
+++ app/src/main/java/com/clk/clkoptionsdialog/MainActivity.java
@@ -0,0 +1,19 @@
+package com.clk.clkoptionsdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+
+import com.clk.optionsdialog.OptionsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
